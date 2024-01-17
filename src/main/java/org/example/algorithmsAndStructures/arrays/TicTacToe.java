c7a037c3ef082494b0245b2744e5a9ef2b57492a
Eisen Valencia <eisenprogramando@gmail.com>
2024-01-17 15:20:56 -0500

    Happy path for Tic tact toe finished

--- src/main/java/org/example/algorithmsAndStructures/arrays/TicTacToe.java
+++ src/main/java/org/example/algorithmsAndStructures/arrays/TicTacToe.java
@@ -1,5 +1,6 @@
 package org.example.algorithmsAndStructures.arrays;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 /*
@@ -37,12 +38,16 @@ public class TicTacToe {
     private String[][] board = {{"0","1","2"}, {"3", "4", "5"}, {"6", "7", "8"}};
     private boolean isXTurn = false;
 
+    private Scanner input = new Scanner(System.in);
+
+    private boolean hasWon = false;
+
         public boolean hasWon(){
             return false;
         }
 
         public void welcome(){
-            String WELCOME_MESSAGE = "Welcome to Eisen's tic tact toe.";
+            String WELCOME_MESSAGE = "Welcome to Eisen's tic tact toe. \n";
             String RULES ="""
                            Here are the rules to play:
                            1. Number of players allowed is 2
@@ -50,6 +55,7 @@ public class TicTacToe {
                            3. To play are used following symbols. First player o, second player x
                            4. To win needs to have 3 symbols in either a row, column or diagonal.
                            5. To do a movement need to add a number between 0 and 8
+                           6. Once a section is used by either symbol can't be replaced.
                            """;
 
             StringBuilder welcomeMessage = new StringBuilder();
@@ -58,33 +64,129 @@ public class TicTacToe {
             welcomeMessage.append(WELCOME_MESSAGE)
                             .append(RULES);
 
-            System.out.println(welcomeMessage.toString());
-            print();
+            System.out.println(WELCOME_MESSAGE);
 
-            // Game starting point
+            System.out.println("Tap enter once you ready to continue with the rules of the game");
+            input.nextLine();
 
+            System.out.println(RULES);
 
         }
 
-        public void addMovement(){
-            String message = isXTurn ? "It's X turn" : "It's O Turn";
-            System.out.print(message);
+        public void play(){
+            welcome();
+            int index = 0;
+            while (!hasWon && index<8){
+                addMovement();
+                print();
+                index++;
+                hasAlreadyWon();
+            }
+        }
 
+        public void addMovement(){
+            String symbol = this.isXTurn ? "'X'": "'O'";
+
+            String message = "It's " + symbol + " turn";
+            System.out.print(message+"\n");
+            print();
+            int[] positions = captureInfo();
+
+            this.board[positions[0]][positions[1]] = symbol;
+
+
+            this.isXTurn = !this.isXTurn;
         }
 
         public void print(){
             for( int i = 0; i < 3; i++){
                 StringBuilder row = new StringBuilder();
                 for (int j = 0; j < 3; j++) {
-                    row.append(board[i][j]);
+                    row.append("  ")
+                            .append(board[i][j])
+                            .append("  ");
                 }
                 System.out.println(row);
             }
         }
 
-        private void captureInfo(){
-            Scanner input = new Scanner(System.in);
-            input.nextInt();
+        private int[] captureInfo(){
+            return translateInput(this.input.nextInt());
         }
 
+        private int[] translateInput(int input){
+            // Validate the value is not greater than 8 or smaller than 0;
+            return new int[]{getRow(input), getColumn(input)};
+        }
+
+        private int getRow(int value) {
+            if(value>2 && value<6){
+                return 1;
+            }
+            if(value>5 && value <=8){
+                return 2;
+            }
+            return 0;
+        }
+        private int getColumn(int value ){
+            return switch (value) {
+                case 0, 3, 6 -> 0;
+                case 1, 4, 7 -> 1;
+                default -> 2;
+            };
+        }
+        private void hasAlreadyWon (){
+           // Validate rows
+            validateRows();
+            validateColumns();
+            validateDiagonals();
+
+        }
+
+        private void validateColumns( ) {
+            if (Objects.equals(this.board[0][0], this.board[1][0]) && Objects.equals(this.board[0][0], this.board[2][0])) {
+                this.hasWon = true;
+                System.out.println("Player " + this.board[0][0] + " has won");
+            }
+
+            if (Objects.equals(this.board[0][1], this.board[1][1]) && Objects.equals(this.board[0][0], this.board[2][1])) {
+                this.hasWon = true;
+                System.out.println("Player " + this.board[0][1] + " has won");
+            }
+
+            if (Objects.equals(this.board[0][2], this.board[1][2]) && Objects.equals(this.board[0][0], this.board[2][2])) {
+                this.hasWon = true;
+                System.out.println("Player " + this.board[0][2] + " has won");
+            }
+
+        }
+
+        private void validateRows(){
+            if (Objects.equals(this.board[0][0], this.board[0][1]) && Objects.equals(this.board[0][0], this.board[0][2])) {
+                this.hasWon = true;
+                System.out.println("Player " + this.board[0][0] + " has won");
+            }
+
+            if (Objects.equals(this.board[1][0], this.board[1][1]) && Objects.equals(this.board[1][0], this.board[1][2])) {
+                this.hasWon = true;
+                System.out.println("Player " + this.board[1][0] + " has won");
+            }
+
+            if (Objects.equals(this.board[2][0], this.board[2][1]) && Objects.equals(this.board[2][0], this.board[2][2])) {
+                this.hasWon = true;
+                System.out.println("Player " + this.board[2][0] + " has won");
+            }
+        }
+
+        private void validateDiagonals(){
+            if (Objects.equals(this.board[0][0], this.board[1][1]) && Objects.equals(this.board[0][0], this.board[2][2])) {
+                this.hasWon = true;
+                System.out.println("Player " + this.board[0][0] + " has won");
+            }
+
+            if (Objects.equals(this.board[2][0], this.board[1][1]) && Objects.equals(this.board[2][0], this.board[0][2])) {
+                this.hasWon = true;
+                System.out.println("Player " + this.board[1][1] + " has won");
+            }
+        }
 }
